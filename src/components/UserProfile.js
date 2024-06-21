5867497fb8b6c069e487ff3d4a47da249109a7e9
Astato <leivagfernando@outlook.com>
2024-06-21 05:30:25 -0300

    Fix incorrect query call in handleSaveProfile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -49,15 +49,12 @@ const UserProfile = ({ setClientLogged, setEditBlog }) => {
 
   const handleSaveProfile = async (image, age, about) => {
     setChangeAbout(false);
+    const query = `image=${DOMPurify.sanitize(image)}${
+      !age ? "" : "&age=" + DOMPurify.sanitize(new Date(age).toISOString())
+    }&about=${DOMPurify.sanitize(about)}`;
     const options = {
       method: "GET",
-      url:
-        BASEURL * "/api/blogs/profile?update=true&image=" +
-        DOMPurify.sanitize(image) +
-        "&age=" +
-        DOMPurify.sanitize(new Date(age).toISOString()) +
-        "&about=" +
-        DOMPurify.sanitize(about),
+      url: BASEURL * "/api/blogs/profile?update=true&" + query,
     };
     try {
       const response = await axios(options);
